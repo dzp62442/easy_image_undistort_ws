24da537f9304c182c55aade4dd12fbba50220f2b
dzp62442 <dzp62442@163.com>
2024-10-23 10:11:06 +0800

    基本实现服务模式去畸变功能

---
+++ src/easy_image_undistort/scripts/test_undistort_client.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+import rospy
+from sensor_msgs.msg import Image
+from easy_image_undistort.srv import ImageUndistort, ImageUndistortRequest, ImageUndistortResponse
+import cv2
+from cv_bridge import CvBridge
+
+ORIGIN_DATA_PATH = '/home/dzp62442/Projects/white_car_demo/origin/0'
+UNDISTORT_DATA_PATH = '/home/dzp62442/Projects/white_car_demo/undistort_ros/0'
+
+def image_service_client():
+    rospy.init_node('image_service_client', anonymous=True)
+    rospy.wait_for_service('/undistort')  # 等待服务可用
+    os.makedirs(UNDISTORT_DATA_PATH, exist_ok=True)
+    print("Start undistort images...")
+
+    imgs_list = os.listdir(ORIGIN_DATA_PATH)
+    imgs_list.sort()
+    
+    for img_name in imgs_list:
+        img_path = os.path.join(ORIGIN_DATA_PATH, img_name)
+        cv_image = cv2.imread(img_path)
+        
+        try:
+            image_service = rospy.ServiceProxy('/undistort', ImageUndistort)  # 创建服务客户端
+            bridge = CvBridge()  # 使用cv_bridge将OpenCV图像转换为ROS图像消息
+            request_image = bridge.cv2_to_imgmsg(cv_image, encoding="bgr8")
+
+            # 创建服务请求
+            request = ImageUndistortRequest()
+            request.request_image = request_image
+
+            # 调用服务
+            response = image_service(request)
+
+            # 从响应中获取图像
+            response_image = bridge.imgmsg_to_cv2(response.response_image, desired_encoding="bgr8")
+            cv2.imwrite(os.path.join(UNDISTORT_DATA_PATH, img_name), response_image)
+
+        except rospy.ServiceException as e:
+            rospy.logerr("Service call failed: %s", e)
+
+
+if __name__ == "__main__":
+    image_service_client()
--- src/easy_image_undistort/src/easy_image_undistort_node.cpp
+++ src/easy_image_undistort/src/easy_image_undistort_node.cpp
@@ -13,11 +13,89 @@
 #include <sensor_msgs/Image.h>
 
 #include "easy_image_undistort/undistorter.h"
+#include "easy_image_undistort/ImageUndistort.h"
+
+using namespace easy_image_undistort;
+
+class EasyImageUndistortNode
+{
+private:
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    ros::Timer timer_;
+	ros::ServiceServer undistort_server_;
+    image_transport::Publisher render_result_pub_;
+    std::shared_ptr<CameraParametersPair> camera_parameters_pair_ptr_;
+    std::shared_ptr<Undistorter> undistorter_ptr_;
+
+public:
+    EasyImageUndistortNode(): nh_("~"), it_(nh_) 
+    {
+        camera_parameters_pair_ptr_ = std::make_shared<CameraParametersPair>(DistortionProcessing::UNDISTORT);
+
+        // 加载输入相机参数
+        std::string input_camera_namespace;
+        nh_.param("input_camera_namespace", input_camera_namespace, std::string("input_camera"));
+        if (!camera_parameters_pair_ptr_->setCameraParameters(nh_, input_camera_namespace, CameraIO::INPUT)) {
+            ROS_FATAL("Loading of input camera parameters failed, exiting");
+            ros::shutdown();
+            exit(EXIT_FAILURE);
+        }
+
+        // 加载输出相机参数
+        double scale_;
+        nh_.param("scale", scale_, 1.0);
+        std::string output_camera_info_source;
+        nh_.param("output_camera_info_source", output_camera_info_source, std::string("input"));
+        if (output_camera_info_source == "ros_params") {
+            std::string output_camera_namespace;
+            nh_.param("output_camera_namespace", output_camera_namespace, std::string("output_camera"));
+            if (!camera_parameters_pair_ptr_->setCameraParameters(nh_, output_camera_namespace, CameraIO::OUTPUT)) {
+                ROS_FATAL("Loading of output camera parameters failed, exiting");
+                ros::shutdown();
+                exit(EXIT_FAILURE);
+            }
+        } 
+        else if (output_camera_info_source == "match_input") {
+            camera_parameters_pair_ptr_->setOutputFromInput(scale_);
+        } 
+        else if (output_camera_info_source == "auto_generated") {
+            camera_parameters_pair_ptr_->setOptimalOutputCameraParameters(scale_);
+        }
+        
+        undistorter_ptr_ = std::make_shared<Undistorter>(*camera_parameters_pair_ptr_);
+        undistort_server_ = nh_.advertiseService("/undistort", &EasyImageUndistortNode::processUndistortRequest, this);  
+        ROS_WARN("Easy image undistort node started .");
+    }
+
+private:
+    // 服务器回调函数，处理请求图像并生成响应图像
+    bool processUndistortRequest(easy_image_undistort::ImageUndistort::Request &req, easy_image_undistort::ImageUndistort::Response &res)
+    {
+        std::string encoding = sensor_msgs::image_encodings::BGR8;
+        sensor_msgs::ImageConstPtr image_msg_ptr = boost::make_shared<sensor_msgs::Image>(req.request_image);
+        cv_bridge::CvImageConstPtr image_in_ptr = cv_bridge::toCvShare(image_msg_ptr, encoding);
+        cv_bridge::CvImagePtr image_out_ptr(new cv_bridge::CvImage(image_in_ptr->header, encoding));
+
+        // Undistort the image
+        undistorter_ptr_->undistortImage(image_in_ptr->image, &(image_out_ptr->image));
+
+        res.response_image = *(image_out_ptr->toImageMsg());
+
+        ROS_WARN("Response one image undistort request .");
+        return true;
+    }
+
+};
+
+
 
 
 int main(int argc, char* argv[]) {
-	// 初始化 ROS
+
 	ros::init(argc, argv, "easy_image_undistort_node");
+
+    EasyImageUndistortNode easy_image_undistort_node;
 
 	ros::spin();
 
